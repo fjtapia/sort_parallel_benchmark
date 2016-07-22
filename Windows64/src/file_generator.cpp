75bb1852b9505a6e9c011f84970644c017111736
fjtapia <fjtapia@gmail.com>
2016-07-22 14:46:28 +0200

    22-july-2016

--- Windows64/src/file_generator.cpp
+++ Windows64/src/file_generator.cpp
@@ -1,58 +1,55 @@
 //----------------------------------------------------------------------------
 /// @file file_generator.cpp
-/// @brief : this program create a file with random information, for to be
-///          used in the benchmarks.
-///          The invocation format is :
-///          file_generator filename number_of_64_bits_elements
+/// @brief This program generte a file with random information, for to be used
+///        in the benchmark programs
 ///
-/// @author Copyright (c) 2010 2015 Francisco José Tapia (fjtapia@gmail.com )\n
+/// @author Copyright (c) 2016 Francisco José Tapia (fjtapia@gmail.com )\n
 ///         Distributed under the Boost Software License, Version 1.0.\n
-///         ( See accompanyingfile LICENSE_1_0.txt or copy at
+///         ( See accompanying file LICENSE_1_0.txt or copy at
 ///           http://www.boost.org/LICENSE_1_0.txt  )
 /// @version 0.1
 ///
 /// @remarks
 //-----------------------------------------------------------------------------
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
-#include <iostream>
 #include <file_vector.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <vector>
-
 
 using std::cout;
 using std::endl;
-namespace bs_tools = boost::sort::parallel::tools;
+namespace bs_util = boost::sort::parallel::detail::util;
 
-void print_banner( );
+void print_banner();
 
-int main ( int argc, char * argv [ ] )
-{   //---------------------------- begin--------------------------------------
+int main(int argc, char *argv[])
+{ //---------------------------- begin--------------------------------------
     std::string name;
     size_t number;
 
-    if ( argc < 3)
-    {   cout<<"This program generate a file filled with random numbers\n";
-        cout<<"of 64 bits\n";
-        cout<<"The invocation format is :\n";
-        cout<<" file_generator file_name number_elements\n\n";
+    if (argc < 3) {
+        cout << "This program generate a file filled with random numbers\n";
+        cout << "of 64 bits\n";
+        cout << "The invocation format is :\n";
+        cout << " file_generator file_name number_elements\n\n";
         return 0;
     };
-    name =  argv [ 1 ];
-    number = atoi ( argv [ 2 ] );
-    if ( number == 0 )
-    {   cout<<"error, the number can't be zero\n";
+    name = argv[1];
+    number = atoi(argv[2]);
+    if (number == 0) {
+        cout << "error, the number can't be zero\n";
         return 0;
     };
 
-    if ( bs_tools::generate_file ( name, number ) != 0)
-        std::cout<<"Error in the file creation\n";
+    if (bs_util::generate_file(name, number) != 0)
+        std::cout << "Error in the file creation\n";
     return 0;
 };
-void print_banner( )
-{   //---------------------------- begin -------------------------------------
-    cout<<" The format of this program is :\n";
-    cout<<" file_generator filename number_elements\n\n";
-    cout<<" The elements are 64 bits random numbers\n";
+void print_banner()
+{ //---------------------------- begin -------------------------------------
+    cout << " The format of this program is :\n";
+    cout << " file_generator number_elements\n\n";
+    cout << " The elements are 64 bits random numbers\n";
 };
